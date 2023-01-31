974536f31f2649af190b0c6cb308a32d663a945d
SilonYT <63094806+SilonYT@users.noreply.github.com>
2023-01-31 15:59:02 +0100

    Delete Adan_CoinCatcher directory

--- Adan_CoinCatcher/GameOverScreen.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;
-
-public class GameOverScreen extends Actor
-{
-    public GameOverScreen()
-    {
-        setImage(new GreenfootImage("Game Over", 48, Color.WHITE, Color.BLACK));
-    }
-}
